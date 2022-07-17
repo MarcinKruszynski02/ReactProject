d01eeaa00011e2709fd373a9907dae01ab9f1c2e
marcin09xd <marcin15xdxd@gmail.com>
2022-07-17 19:56:09 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+//const h1 = document.createElement("h1")
+//h1.textContent = "Hello world"
+//h1.className = "header"
+//console.log(h1);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// <h1 class="header">
+//const element = <h1 className="header">This is JSX</h1>
+//console.log(element)
+
+//JSX
+
+const up = 
+(
+<div>  
+    <div>
+       <h1>Kiera chuj</h1>
+   </div>
+   <div>
+        <ul>
+            <li>yo</li>
+            <li>dupa</li>
+        </ul>
+   </div>
+   <div>
+    <h3>yoo</h3>
+   </div>
+   <div>
+    <h5>yoos</h5>
+   </div>
+
+ </div>  
+   
 );
+ReactDOM.render(up,document.getElementById("root"))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+    
+
